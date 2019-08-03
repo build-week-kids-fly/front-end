f8e72f26639b50f86d31d4fbedf1cbe655e9dc6e
benedictify <benedictify@gmail.com>
2019-08-02 20:45:18 -0500

    remove unnecessary items from cra

--- kids-fly-app/src/App.js
+++ kids-fly-app/src/App.js
@@ -3,24 +3,12 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+      </div>
+    )
+  }
 }
 
 export default App;
